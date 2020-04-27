2107fac0ee292b840b5a3494998997b2273cb7ea
TJ's Macbook <iamtylerjordan@gmail.com>
2020-04-27 07:36:05 -0400

    header styles

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,15 +2,37 @@ import React from 'react';
 import './header.scss';
 
 class Header extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuOpen: false,
+            menuButtonActive: false
+        }
+    }
+
+    // this.setState({
+    //     menuOpen: menuOpen,
+    //     menuButtonActive: menuButtonActive
+    // })
+
+    // menuToggle() {
+    //     var isMenuBtnActive = this.state.menuButtonActive,
+    //         isMenuOpen = this.state.menuOpen;
+
+    //     isMenuBtnActive ? this.setState({ menuButtonActive: true }) : this.setState({ menuButtonActive: false });
+    // }
+
+
     render() {
         return (
             <div className="header">
+                <div className="menu-btn" onClick={this.menuToggle}>
+                    <div className="menu-slice"></div>
+                    <div className="menu-slice"></div>
+                    <div className="menu-slice"></div>
+                </div>
                 <div className="main-nav">
-                    <div className="menu-btn">
-                        <div className="menu-slice"></div>
-                        <div className="menu-slice"></div>
-                        <div className="menu-slice"></div>
-                    </div>
                     <div className="main-menu">
                         <ul>
                             <li>Menu Item 1</li>
